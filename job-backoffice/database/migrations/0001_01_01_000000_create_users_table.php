d7fda117b4f327c2007be3ee4cf9fcb4050e8772
Ammar-1993 <ammaralnggar@gmail.com>
2025-10-04 21:37:36 +0300

    Added migration tables.

--- job-backoffice/database/migrations/0001_01_01_000000_create_users_table.php
+++ job-backoffice/database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,13 +12,17 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            // $table->id();
+            $table->uuid('id')->primary();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            //Admin, Company Owner, Job Seeker
+            $table->enum('role', ['admin', 'company_owner', 'job_seeker'])->default('job_seeker');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
