f31d28790d3c9ce83838fa65b2a9682efef5a7bd
Ammar-1993 <ammaralnggar@gmail.com>
2025-11-25 08:02:50 +0300

    Quick accessibility fixes applied.

--- job-backoffice/resources/views/components/nav-link.blade.php
+++ job-backoffice/resources/views/components/nav-link.blade.php
@@ -2,8 +2,8 @@
 
 @php
     $classes = ($active ?? false)
-        ? 'flex items-center px-4 py-2 w-full text-sm font-medium text-gray-900 bg-indigo-100 rounded-md focus:outline-none hover:bg-gray-100 focus:bg-gray-200 transition duration-150 ease-in-out'
-        : 'flex items-center px-4 py-2 w-full text-sm font-medium text-gray-500 hover:text-gray-700 hover:bg-gray-100 rounded-md focus:outline-none focus:bg-gray-200 transition duration-150 ease-in-out';
+        ? 'flex items-center px-4 py-2 w-full text-sm font-medium text-gray-900 bg-indigo-100 rounded-md hover:bg-gray-100 transition duration-150 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
+        : 'flex items-center px-4 py-2 w-full text-sm font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-100 rounded-md transition duration-150 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2';
 @endphp
 
 <li>
